befe458aa0ecf45552f9da178e6bd6df40da1e7e
unknown <Imanbriyan@gmail.com>
2024-07-05 23:54:27 +0700

    Add simple content on HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
-import Image from 'next/image';
+import React from 'react';
+import { FiDownload } from 'react-icons/fi';
+import { Button } from '@/components/ui/button';
 
-export default function Home() {
+import { HOME_HEADING_INFO } from '@/src/constant/constant';
+
+const HomePage = () => {
   return (
-    <div>
-      <main>
-        <h1 className="font-semibold text-8xl text-center mt-64">COMING SOON HOMEPAGE</h1>
-      </main>
-    </div>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">{HOME_HEADING_INFO.role}</span>
+            <h1 className="h1 mb-6">
+              {HOME_HEADING_INFO.headingText} <br />
+              <span className="text-accent">{HOME_HEADING_INFO.name}</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">{HOME_HEADING_INFO.description}</p>
+            <div className="flex flex-col  xl:flex-row items-center gap-8">
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default HomePage;
--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -20,3 +20,11 @@ export const links = [
         path: '/contact'
     },
 ]
+
+export const HOME_HEADING_INFO = {
+    name: 'Briyan Sitinjak',
+    role: 'Frontend - WordPress Developer',
+    headingText: "Hello I'm",
+    description:
+        'As a developer with more than 4 years of experience with coding and project development, I believe I can add value to the team to accomplish its goals. I have always been interested in the development of using the latest technology and would love to put my ideas and thinking into the project'
+};

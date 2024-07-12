5bfa4a3c9f893a1c934dbd0154840bfe0067f6e3
unknown <Imanbriyan@gmail.com>
2024-07-12 23:31:18 +0700

    handle duration animation

--- components/Stats/Stats.tsx
+++ components/Stats/Stats.tsx
@@ -50,7 +50,7 @@ const Stats = () => {
               >
                 <CountUp
                   end={data.num}
-                  duration={5}
+                  duration={4}
                   delay={2}
                   className="text-4xl xl:text-6xl font-extrabold px-4 py-2"
                 />

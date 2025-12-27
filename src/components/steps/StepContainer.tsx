9d708005beb5fcfb801ab60a5d098b74ad99b75f
BriyanSitinjak <Imanbriyan@gmail.com>
2025-12-27 23:57:28 +0700

    handle mobile version

--- src/components/steps/StepContainer.tsx
+++ src/components/steps/StepContainer.tsx
@@ -21,7 +21,7 @@ const StepContainer = () => {
   };
 
   return (
-    <div className="block md:hidden w-80 h-[800px] bg-gray-50 border border-gray-200 rounded-lg p-4 shadow-md mx-auto overflow-auto">
+    <div className="block md:hidden w-80 h-[800px] bg-blue-50 border border-gray-200 rounded-lg p-4 shadow-md mx-auto overflow-auto">
       <div className="w-full h-full flex items-center justify-center">
         {renderComponent()}
       </div>

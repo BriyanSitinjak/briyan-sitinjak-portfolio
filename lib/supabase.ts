35f234c94eeaf08d5d33756c87e0ef640dc678d8
BriyanSitinjak <Imanbriyan@gmail.com>
2026-01-02 23:31:43 +0700

    integrate the supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://klrikfzixuzidskulbzd.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtscmlrZnppeHV6aWRza3VsYnpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwODUzMTIsImV4cCI6MjA4MjY2MTMxMn0.aLw4m28u58aCWye8l-i_jUqs2dvdsSjNwLKt4lgwX3M";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);

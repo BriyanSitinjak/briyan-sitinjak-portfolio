45390a3fe1b27ccc4c031eecdc844ddb5a16fa12
BriyanSitinjak <Imanbriyan@gmail.com>
2026-02-11 23:59:16 +0700

    added

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -120,7 +120,7 @@ export const LIST_EXPERIENCES: IExperiencesData[] = [
     },
     {
         name: 'Fairpax',
-        altText: 'fairpax_web',
+        altText: 'fairpaxindonesia',
         description: 'Web commerce for selling products using WooCommerce and WordPress and implement the payment gateway and shipping using rajaongkir',
         img: kauluConceptWeb,
         link: 'https://fairpaxindonesia.com/'
